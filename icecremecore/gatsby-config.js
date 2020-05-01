dcde9055c042426eedbc143acf6ed597a313131f
supremeicecreme <thebullasjr@gmail.com>
2020-05-01 21:37:50 +0100

    Update gatsby-config.js

--- icecremecore/gatsby-config.js
+++ icecremecore/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `IceCremeCore`,
+    description: `The home of Supreme.Icecreme`,
+    author: `Supreme.Icecreme`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `icecremecore`,
+        short_name: `icc`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#e3292a`,
+        theme_color: `#e3292a`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/SupremeIcecreme.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
