46cc4b6dbe3ee1bf8fb5fd371262a891f89fc98c
supremeicecreme <thebullasjr@gmail.com>
2020-05-01 21:39:35 +0100

    Haven't worked out how to safely get rid of this guy

--- icecremecore/src/pages/page-2.js
+++ icecremecore/src/pages/page-2.js
@@ -4,13 +4,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const IndexPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    </div>
   </Layout>
 )
 
-export default SecondPage
+export default IndexPage
