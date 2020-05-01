8e11a12288235ac3d477de28c93e1ad7d929f82a
supremeicecreme <thebullasjr@gmail.com>
2020-05-01 21:39:16 +0100

    Set up homepage

--- icecremecore/src/pages/index.js
+++ icecremecore/src/pages/index.js
@@ -2,19 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <header class="d-flex align-items-center">
+      <div class="container flex-column text-light mb-5 pb-3">
+        <h1 class="display-4">Hello World!</h1>
+        <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis doloremque mollitia corrupti optio hic deserunt quasi incidunt dolorum ipsam, aut ducimus? Ex, soluta? Totam laudantium expedita doloribus dicta, nemo aliquam!</p>
+      </div>  
+    </header>
+    <section>
+      <div class="container pt-5">
+        <h3>Who am I?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime eligendi voluptatem id cum consectetur rerum, voluptates tempore impedit corrupti qui ducimus nostrum, enim, ut pariatur molestiae ullam. Iste fugiat doloribus inventore cumque ea cum libero pariatur iusto. Nemo fugit facilis officia harum neque dignissimos voluptatibus, earum porro odit hic quos cum aut minus natus cumque commodi non tempore officiis facere est expedita, necessitatibus repudiandae dolores. Ratione impedit consectetur esse. Temporibus alias ipsa tempore consectetur nesciunt minima recusandae saepe modi iste nisi. Accusantium illum consequatur cupiditate quod quam eveniet libero corrupti accusamus voluptatibus voluptates unde vero soluta harum culpa, fugiat temporibus? Provident earum ipsum porro similique molestiae atque debitis fugiat molestias harum ex iste accusantium vel, assumenda facilis ipsam aut quidem.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime eligendi voluptatem id cum consectetur rerum, voluptates tempore impedit corrupti qui ducimus nostrum, enim, ut pariatur molestiae ullam. Iste fugiat doloribus inventore cumque ea cum libero pariatur iusto. Nemo fugit facilis officia harum neque dignissimos voluptatibus, earum porro odit hic quos cum aut minus natus cumque commodi non tempore officiis facere est expedita, necessitatibus repudiandae dolores. Ratione impedit consectetur esse. Temporibus alias ipsa tempore consectetur nesciunt minima recusandae saepe modi iste nisi. Accusantium illum consequatur cupiditate quod quam eveniet libero corrupti accusamus voluptatibus voluptates unde vero soluta harum culpa, fugiat temporibus? Provident earum ipsum porro similique molestiae atque debitis fugiat molestias harum ex iste accusantium vel, assumenda facilis ipsam aut quidem.</p>
+      </div>
+    </section>
   </Layout>
 )
 
