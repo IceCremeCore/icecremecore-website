9a9da8499be2e166f5c307b72425f177e0f6c9b5
supremeicecreme <thebullasjr@gmail.com>
2020-05-06 21:46:33 +0100

    Added proper JSX syntax

--- icecremecore/src/components/navbar.js
+++ icecremecore/src/components/navbar.js
@@ -4,18 +4,18 @@ import React from "react"
 import BrandImage from "./BrandImage"
 
 const Navbar = ({ siteTitle }) => (
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-gradient">
-    <div class="container">
-      <a href="#" class="brand">
+  <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-gradient">
+    <div className="container">
+      <Link to="/" className="brand">
           <BrandImage /> {siteTitle}
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item" id="nav-home"><Link to="/" className="nav-link" activeClassName="active">Home</Link></li>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item" id="nav-home"><Link to="/" className="nav-link" activeClassName="active">Home</Link></li>
         </ul>
       </div>
     </div>
