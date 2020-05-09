7d31241987225530b1e5bcf10fe4b30a53bba166
supremeicecreme <thebullasjr@gmail.com>
2020-05-09 16:39:17 +0100

    Create blog.js

---
+++ icecremecore/src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+import SEO from "../components/seo"
+import Layout from "../components/layout/page"
+
+const BlogHome = () => (
+    <Layout>
+        <SEO title="Blog" />
+        <h3>Blog</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sunt voluptate maxime repellendus officiis quis animi tempora quia saepe id! Mollitia autem debitis quasi, veritatis repudiandae ratione dolores quisquam obcaecati? Mollitia, iure? Obcaecati, sit. Minus magnam unde facilis odio culpa quibusdam, consectetur obcaecati quia doloremque eos, est, ex debitis aliquid?</p>
+    </Layout>
+)
+
+export default BlogHome
