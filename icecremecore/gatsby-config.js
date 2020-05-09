54792757fe742a09b5c632af4a5363a4e56a5a08
supremeicecreme <thebullasjr@gmail.com>
2020-05-09 22:00:26 +0100

    Update gatsby-config.js

--- icecremecore/gatsby-config.js
+++ icecremecore/gatsby-config.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv").config()
+
+
 module.exports = {
   siteMetadata: {
     title: `IceCremeCore`,
@@ -10,7 +13,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -32,15 +35,11 @@ module.exports = {
       options: {
         spaceId: `xucp5pnnv1sk`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      }
-    }
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
 }
-
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
