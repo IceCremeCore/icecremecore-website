127752b8c985438149f70144a3d976818603ede1
supremeicecreme <thebullasjr@gmail.com>
2020-05-10 13:32:15 +0100

    Added blog post creation

--- icecremecore/gatsby-node.js
+++ icecremecore/gatsby-node.js
@@ -1,7 +1,54 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    const blogPost = path.resolve(`./src/templates/post.js`)
+    return graphql(`
+        {
+            allContentfulBlogPost{
+                edges{
+                    node{
+                        title
+                        slug
+                        description{
+                            internal{
+                                content
+                            }
+                        }
+                        content{
+                            childContentfulRichText{
+                                html
+                            }
+                        }
+                        author{
+                            name
+                        }
+                        publishDate
+                        tags
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        if(result.errors) {
+            throw result.errors
+        }
+
+        const posts = result.data.allContentfulBlogPost.edges
+
+        posts.forEach((post, index) => {
+            const previous = index === posts.length - 1 ? null : posts[index - 1].node
+            const next = index === 0 ? null : posts[index - 1].node
+
+            createPage({
+                path: `blog/` + post.node.slug,
+                component: blogPost,
+                context: {
+                    slug: `blog/` + post.node.slug,
+                    previous,
+                    next,
+                },
+            })
+        })
+    })
+}
