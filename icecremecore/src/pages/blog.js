ea0808b11e163334ed0a437518add397bb18ee6d
supremeicecreme <thebullasjr@gmail.com>
2020-05-10 18:12:58 +0100

    Update blog.js

--- icecremecore/src/pages/blog.js
+++ icecremecore/src/pages/blog.js
@@ -1,13 +1,58 @@
 import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout/page"
+import { Link, graphql } from "gatsby"
 
-const BlogHome = () => (
+const BlogHome = ({data}) => (
     <Layout>
         <SEO title="Blog" />
         <h3>Blog</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sunt voluptate maxime repellendus officiis quis animi tempora quia saepe id! Mollitia autem debitis quasi, veritatis repudiandae ratione dolores quisquam obcaecati? Mollitia, iure? Obcaecati, sit. Minus magnam unde facilis odio culpa quibusdam, consectetur obcaecati quia doloremque eos, est, ex debitis aliquid?</p>
+        <p className="lead">This is a blog.</p>
+        <div className="posts">
+            {data.allContentfulBlogPost.edges.map(post => (
+                <div className="card mb-3" key={post.node.id}>
+                    <div className="card-body">
+                        <h5 className="mb-1"><Link to={`blog/`+post.node.slug} className="card-link text-dark">{post.node.title}</Link></h5>
+                        <p className="m-0"><small className="card-text text-muted">by {post.node.author.name} on {post.node.publishDate}</small></p>
+                        <p className="card-text">{post.node.description.internal.content}</p>
+                        <div className="d-flex justify-content-around">
+                            <div className="d-flex">
+                                Tags: 
+                                {post.node.tags.map(tag => (
+                                    <div className="mx-1">{tag}</div>
+                                ))}
+                            </div>
+                            <Link to={`blog/`+post.node.slug} className="ml-auto">Read more <i className="fas fa-arrow-right"></i></Link>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
     </Layout>
 )
+
+export const pageQuery = graphql`
+    query GetBlogPosts {
+        allContentfulBlogPost {
+            edges {
+                node {
+                    id
+                    title
+                    author {
+                        name
+                    }
+                    publishDate
+                    slug
+                    description {
+                        internal {
+                            content
+                        }
+                    }
+                    tags
+                }
+            }
+        }
+    }
+`
 
 export default BlogHome
